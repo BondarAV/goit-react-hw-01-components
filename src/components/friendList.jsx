0970ea60cca02d7924fdf4835a2ba915f08523e6
Andrii Bondar <77549085+BondarAV@users.noreply.github.com>
2023-09-14 20:52:05 +0300

    Final fix

--- src/components/friendList.jsx
+++ src/components/friendList.jsx
@@ -3,7 +3,7 @@ import FriendListItem from "./friendListItem";
 export default function FriendList({friends}) {
   return (
     <ul className="friend-list">
-      {friends.map((friend) => (<FriendListItem {...friend} />))}
+      {friends.map((friend) => (<FriendListItem {...friend} key={friend.id} />))}
     </ul>
   );
 }
